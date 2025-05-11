b5d461a26a3b669598b1bcb901b4d5947f00a4d8
Guillermo Tantaleán <guillermo.rs2017@gmail.com>
2025-05-11 09:37:59 -0500

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Necesario para generar archivos estáticos
+  basePath: '/nombre-de-tu-repositorio', // Reemplaza con el nombre de tu repositorio
+  images: {
+    unoptimized: true, // Necesario para exportación estática
+  },
 };
 
 export default nextConfig;
